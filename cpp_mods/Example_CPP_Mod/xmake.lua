4ad6a9267cab503df15ce7ad56403a23f934cbc2
Mythical-Github <MythicalData@gmail.com>
2024-08-04 05:04:58 -0400

    added example cpp mod

---
+++ cpp_mods/Example_CPP_Mod/xmake.lua
@@ -0,0 +1,20 @@
+
+function add_includedirs_recursively(baseDir)
+    add_includedirs(baseDir, {public = true})
+
+    for _, dir in ipairs(os.dirs(baseDir .. "/*")) do
+        add_includedirs(dir, {public = true})
+        add_includedirs_recursively(dir)
+    end
+end
+
+local currentDir = os.curdir()
+local projectName = path.filename(currentDir)
+
+target(projectName)
+
+    add_rules("ue4ss.mod")
+
+    add_includedirs_recursively(currentDir)
+
+    add_files("**.cpp")
